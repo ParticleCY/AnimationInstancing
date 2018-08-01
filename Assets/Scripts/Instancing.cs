bf30a331ee01588f04ffa051ed95f6e9d65080a7
ParticleCY <chengyun0407@126.com>
2018-08-01 08:56:14 +0800

    Initial commit

---
+++ Assets/Scripts/Instancing.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+
+public class Instancing : MonoBehaviour
+{
+    public Animator animator = null;
+    public Transform Pose;
+    public GameObject prototype;
+    
+    
+    /*
+    public class InstanceAnimationInfo 
+    {
+          public List<AnimationInfo> listAniInfo;
+          public ExtraBoneInfo extraBoneInfo;
+    }
+    public class AnimationInfo
+    {
+        public string animationName;
+        public int animationNameHash;
+        public int totalFrame;
+        public int fps;
+        public int animationIndex;
+        public int textureIndex;
+        public bool rootMotion;
+        public WrapMode wrapMode;
+        public Vector3[] velocity;
+        public Vector3[] angularVelocity;
+        public List<AnimationEvent> eventList; 
+    }
+
+    public class ExtraBoneInfo
+    {
+        public string[] extraBone;
+        public Matrix4x4[] extraBindPose;
+    }
+    */
+    public void InitializeAnimation()
+    {
+        if (prototype == null)
+        {
+            Debug.LogError("The prototype is NULL. Please select the prototype first.");
+        }
+        Debug.Assert(prototype != null);
+        GameObject thisPrefab = prototype;
+        List<Matrix4x4> bindPose = new List<Matrix4x4>(150);
+        //bindPose information can be achieve from any lodInfo.skinnedMeshRenderer
+        Transform[] bones = RuntimeHelper.MergeBone(lodInfo[0].skinnedMeshRenderer, bindPose);
+
+    }
+
+    public class LodInfo
+    {
+        public int lodLevel;
+        public SkinnedMeshRenderer[] skinnedMeshRenderer;
+        public MeshRenderer[] meshRenderer;
+        public MeshFilter[] meshFilter;
+        public InstancingMgr.VertexCache[] vertexCacheList;
+    }
+
+    public LodInfo[] lodInfo;
+    
+    /*
+     * In Fn Start, we do
+     * 1. If the Obj has an Animator, then disable it.
+     * 2. Import meshes of the Obj into LodInfo[]. (defautly in LodInfo[0])
+     */
+    void Start()
+    {
+        LODGroup lod = GetComponent<LODGroup>();
+        animator = GetComponent<Animator>();
+        Pose = GetComponent<Transform>();
+        if (InstancingMgr.Instance.UseInstancing && animator != null)
+        {
+            animator.enabled = false;
+        }
+
+        if (lod != null)
+        {
+            //if an Obj already has lodlevel model, then import it.
+            lodInfo = new LodInfo[lod.lodCount];
+            LOD[] lods = lod.GetLODs();
+            for (int i = 0; i != lods.Length; ++i)
+            {
+                if (lods[i].renderers == null)
+                {
+                    continue;
+                }
+
+                LodInfo info = new LodInfo();
+                info.lodLevel = i;
+                info.vertexCacheList = new InstancingMgr.VertexCache[lods[i].renderers.Length];
+                List<SkinnedMeshRenderer> listSkinnedMeshRenderer = new List<SkinnedMeshRenderer>();
+                List<MeshRenderer> listMeshRenderer = new List<MeshRenderer>();
+                foreach (var render in lods[i].renderers)
+                {
+                    if (render is SkinnedMeshRenderer)
+                        listSkinnedMeshRenderer.Add((SkinnedMeshRenderer) render);
+                    if (render is MeshRenderer)
+                        listMeshRenderer.Add((MeshRenderer) render);
+                }
+
+                info.skinnedMeshRenderer = listSkinnedMeshRenderer.ToArray();
+                info.meshRenderer = listMeshRenderer.ToArray();
+                //todo, to make sure whether the MeshRenderer can be in the LOD.
+                info.meshFilter = null;
+                for (int j = 0; j != lods[i].renderers.Length; ++j)
+                {
+                    lods[i].renderers[j].enabled = false;
+                }
+
+                lodInfo[i] = info;
+            }
+        }
+        else
+        {
+            //else defaul import meshes in lodlevel 0
+            lodInfo = new LodInfo[1];
+            LodInfo info = new LodInfo();
+            info.lodLevel = 0;
+            info.skinnedMeshRenderer = GetComponentsInChildren<SkinnedMeshRenderer>();
+            info.meshRenderer = GetComponentsInChildren<MeshRenderer>();
+            info.meshFilter = GetComponentsInChildren<MeshFilter>();
+            info.vertexCacheList =
+                new InstancingMgr.VertexCache[info.skinnedMeshRenderer.Length + info.meshRenderer.Length];
+            lodInfo[0] = info;
+
+            for (int j = 0; j != info.meshRenderer.Length; ++j)
+            {
+                info.meshRenderer[j].enabled = false;
+            }
+
+            for (int j = 0; j != info.skinnedMeshRenderer.Length; ++j)
+            {
+                info.skinnedMeshRenderer[j].enabled = false;
+            }
+        }
+        InstancingMgr.Instance.AddInstance(gameObject);
+    }
+}
---
+++ Assets/Scripts/InstancingMgr.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class InstancingMgr : Singleton<InstancingMgr>
+{
+    //One BoneTextureInfo saves the BoneTexture of a certain Prefeb with "Name"
+    public struct BoneTextureInfo
+    {
+        public string Name;
+        public int BlockWidth;
+        public int BlockHeight;
+        public Texture2D[] BoneTexture;
+    }
+
+    //InstanceData saves the Infos to render a certain Prefeb
+    public class InstanceData
+    {
+        public List<Matrix4x4[]>[] WorldMatrix;
+        public List<float[]>[] FrameIndex;
+        public List<float[]>[] PreFrameIndex;
+        public List<float[]>[] TransitionProgress;
+    }
+
+    public class VertexCache
+    {
+        public int nameCode;
+        public Mesh mesh = null;
+        public InstanceData instanceData;
+        public Vector4[] weight;
+        public Vector4[] boneIndex;
+        public Material[] materials = null;
+        public Matrix4x4[] bindPose;
+        public Transform[] bonePose;
+        public int boneTextureIndex = -1;
+
+        public class InstancingPackage
+        {
+            public Material[] material;
+            public int animationTextureIndex = 0;
+            public int subMeshCount = 1;
+            public int instancingCount;
+            public int size;
+            public MaterialPropertyBlock propertyBlock;
+        }
+
+        public int packageIndex = 0;
+        public int[] runtimePackageIndex;
+
+        // array[index base on texture][package index]
+        public List<InstancingPackage>[] packageList;
+    }
+
+
+    private bool _useInstancing = true;
+
+    public bool UseInstancing
+    {
+        get { return _useInstancing; }
+        set { _useInstancing = value; }
+    }
+
+    public class InstancePkg
+    {
+        InstanceData Data;
+        Instancing Script;
+    }
+
+    public string FileName;
+    public string prefebName;
+
+    private List<BoneTextureInfo> _boneTextrueInfo = new List<BoneTextureInfo>();
+    private List<Instancing> _InstancingData = new List<Instancing>();
+
+    private void OnEnable()
+    {
+        UseInstancing = true;
+    }
+
+    public void AddInstance(GameObject obj)
+    {
+        //TODO
+        Instancing script = obj.GetComponent<Instancing>();
+        script.InitializeAnimation();
+        _InstancingData.Add(script);
+    }
+
+    public void DistroyInstace(Instancing instance)
+    {
+        //TODO
+    }
+
+    void Start()
+    {
+        
+        FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+        BinaryReader reader = new BinaryReader(fs);
+        LoadBoneTexture(reader);
+        fs.Close();
+    }
+
+    void Update()
+    {
+        generateBoneMatrixData();
+        render();
+    }
+
+    private void generateBoneMatrixData()
+    {
+        //TODO
+    }
+
+    private void render()
+    {
+        //TODO
+    }
+
+    public void clear()
+    {
+        _boneTextrueInfo.Clear();
+    }
+
+    //private LoadBoneTexture(BinaryReader reader, string prefebName)
+    private void LoadBoneTexture(BinaryReader reader)
+    {
+        TextureFormat format = TextureFormat.RGBAHalf;
+        int count = reader.ReadInt32();
+        int blockWidth = reader.ReadInt32();
+        int blockHeight = reader.ReadInt32();
+        BoneTextureInfo aniTexture = new BoneTextureInfo();
+        aniTexture.BoneTexture = new Texture2D[count];
+        aniTexture.Name = prefebName;
+        aniTexture.BlockWidth = blockWidth;
+        aniTexture.BlockHeight = blockHeight;
+        for (int i = 0; i != count; ++i)
+        {
+            int textureWidth = reader.ReadInt32();
+            int textureHeight = reader.ReadInt32();
+            int byteLength = reader.ReadInt32();
+            byte[] b = new byte[byteLength];
+            b = reader.ReadBytes(byteLength);
+            Texture2D texture = new Texture2D(textureWidth, textureHeight, format, false);
+            texture.LoadRawTextureData(b);
+            texture.filterMode = FilterMode.Point;
+            texture.Apply();
+            aniTexture.BoneTexture[i] = texture;
+            FileStream debugfile = File.Open("D:/Format/3DsInput.txt", FileMode.Create);
+            StreamWriter debugwriter = new StreamWriter(debugfile);
+            for (int x = 0; x < textureWidth; x += 4)
+            {
+                for (int y = 0; y < textureHeight; y++)
+                {
+                    debugwriter.Write(string.Format(
+                        "{0},{1}:[{2};{3};{4};{5}]\r\n", x, y,
+                        texture.GetPixel(x, y).ToString(), texture.GetPixel(x + 1, y).ToString(),
+                        texture.GetPixel(x + 2, y).ToString(),
+                        texture.GetPixel(x + 3, y).ToString()));
+                }
+            }
+
+            debugwriter.Close();
+        }
+
+        _boneTextrueInfo.Add(aniTexture);
+    }
+}
